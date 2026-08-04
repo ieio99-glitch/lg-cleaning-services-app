1402196f12e832e92756bbad163799edfa8597bc
Luis <278548991+ieio99-glitch@users.noreply.github.com>
2026-08-03 20:30:45 -0600

    Initial Commit

---
+++ LG Cleaning Owner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LG Cleaning Owner
+//
+//  Created by user on 8/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LG Cleaning Owner/LG_Cleaning_OwnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LG_Cleaning_OwnerApp.swift
+//  LG Cleaning Owner
+//
+//  Created by user on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LG_Cleaning_OwnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
